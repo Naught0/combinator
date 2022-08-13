e908a90569c47cec020fff80edfab3996ee94005
James <4589250+Naught0@users.noreply.github.com>
2022-08-12 23:16:04 -0400

    Remove auto-load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,12 +23,6 @@ export const App = () => {
     }
   }, []);
 
-  useEffect(() => {
-    if (deckUrl && !deckData) {
-      findCombos();
-    }
-  }, [deckUrl, deckData]);
-
   const findCombos = (url?: string) => {
     setDeckData(undefined);
     setError(undefined);

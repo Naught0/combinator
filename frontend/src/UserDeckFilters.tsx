ae9630217ff64b2a899c727814173289e29970f5
James <4589250+Naught0@users.noreply.github.com>
2022-11-12 02:04:33 -0500

    WIP Filtering

--- frontend/src/UserDeckFilters.tsx
+++ frontend/src/UserDeckFilters.tsx
@@ -1,23 +1,25 @@
 import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
-import { faSortAsc, faSortDesc } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 import { Dropdown } from "./Dropdown";
-import { SortDirection } from "./UserDecksContainer";
+import { SortDirection, sortDirIconMap } from "./UserDecksContainer";
 
 interface Props {
   setSortBy: (k: keyof Deck) => void;
   setSortDir: (k: SortDirection) => void;
   titleFilter?: string;
+  sortDirection: SortDirection;
   setFormatFilter: (key: string) => void;
   resetFilters: () => void;
   setTitleFilter: (s: string) => void;
 }
 export const UserDeckFilters: FC<Props> = ({
-  setSortBy,
   titleFilter,
+  sortDirection,
+  setSortBy,
   setTitleFilter,
   setFormatFilter,
+  setSortDir,
   resetFilters,
 }) => {
   return (
@@ -51,23 +53,35 @@ export const UserDeckFilters: FC<Props> = ({
         </div>
         <div className="level">
           <div className="level-item">
-            <Dropdown title={"Sort by"} hoverable>
-              {["lastUpdatedAtUtc", "createdAtUtc"].map((s) => (
-                <a
-                  className="dropdown-item"
-                  key={`dropdown-sort-opt-${s}`}
-                  onClick={() => setSortBy(s as keyof Deck)}
+            <div className="field is-horizontal">
+              <div className="control">
+                <Dropdown title={"Sort by"} hoverable>
+                  {["lastUpdatedAtUtc", "createdAtUtc"].map((s) => (
+                    <a
+                      className="dropdown-item"
+                      key={`dropdown-sort-opt-${s}`}
+                      onClick={() => setSortBy(s as keyof Deck)}
+                    >
+                      {s}
+                    </a>
+                  ))}
+                </Dropdown>
+                <button
+                  className="button"
+                  onClick={() =>
+                    setSortDir(
+                      sortDirection === SortDirection.ASC
+                        ? SortDirection.DESC
+                        : SortDirection.ASC
+                    )
+                  }
                 >
-                  {s}
-                </a>
-              ))}
-            </Dropdown>
-          </div>
-          <div className="level-item">
-            <div className="buttons">
-              <button className="button">
-                <span></span>
-              </button>
+                  <span className="icon">
+                    {sortDirIconMap.get(sortDirection)}
+                  </span>
+                  <span>{sortDirection.toString()}</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>

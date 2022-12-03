b8069eb32141c4a052f678e88f7019b137306109
James <4589250+Naught0@users.noreply.github.com>
2022-12-03 06:25:32 -0500

    Remove unused import

--- frontend/src/ComboContainer.tsx
+++ frontend/src/ComboContainer.tsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC, ReactNode, useMemo, useState } from "react";
 import { toast } from "react-toastify";
 import { Combo } from "./Combo";
-import { IconText } from "./IconText";
 
 enum Tab {
   COMBOS,

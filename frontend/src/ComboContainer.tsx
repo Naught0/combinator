47fbc1d19b644fbb5cdfc78f686c31058f1e75b9
James <4589250+Naught0@users.noreply.github.com>
2022-12-03 06:38:13 -0500

    Cease to be an a11y

--- frontend/src/ComboContainer.tsx
+++ frontend/src/ComboContainer.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import { faShare, faSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC, ReactNode, useMemo, useState } from "react";

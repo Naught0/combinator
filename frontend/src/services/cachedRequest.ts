ffb771a5a971c836ddfc40b37d434f629a8c35fe
James E <4589250+Naught0@users.noreply.github.com>
2024-01-08 01:54:31 -0500

    Cache post requests

--- frontend/src/services/cachedRequest.ts
+++ frontend/src/services/cachedRequest.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { setupCache } from "axios-cache-adapter";
 
-const cache = setupCache({ maxAge: 10 * 60 * 1000, exclude: { query: false } });
+const cache = setupCache({
+  maxAge: 5 * 60 * 1000,
+  exclude: { query: false, methods: ["put", "patch", "delete"] },
+});
 
 const cachedClient = axios.create({
   adapter: cache.adapter,

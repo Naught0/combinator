cce7b16d91a918041626933cc4c5cee89d0b503a
James <4589250+Naught0@users.noreply.github.com>
2024-01-08 04:45:59 -0500

    Google analytics (#22)
    * Ignore node_modules
    
    * Add google analytics
    
    * Fix deps issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import eslint from "vite-plugin-eslint";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 export default defineConfig({
   base: "",
-  plugins: [react(), viteTsconfigPaths(), eslint({ useEslintrc: true })],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    eslint(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-N2BPB5VE3D",
+      },
+    }),
+  ],
   server: {
     open: true,
     port: 3000,

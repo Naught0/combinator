ebc018418ebae83cceda93183193afd834d15135
James E <4589250+Naught0@users.noreply.github.com>
2024-01-09 15:25:53 -0500

    Use dynamic api url

--- frontend/src/services/cachedRequest.ts
+++ frontend/src/services/cachedRequest.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { setupCache } from "axios-cache-interceptor";
 
-const instance = axios.create();
+const instance = axios.create({
+  url: import.meta.env.VITE_API_URL,
+});
 const cachedClient = setupCache(instance, {
   methods: ["post", "get"],
   interpretHeader: true,

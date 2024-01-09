8c4cd58b38ea56713c24ea8b71a17eff46f8731b
James E <4589250+Naught0@users.noreply.github.com>
2024-01-09 18:02:01 -0500

    Use base url env properly

--- frontend/src/services/cachedRequest.ts
+++ frontend/src/services/cachedRequest.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setupCache } from "axios-cache-interceptor";
 
 const instance = axios.create({
-  url: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 const cachedClient = setupCache(instance, {
   methods: ["post", "get"],

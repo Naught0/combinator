55c9fcade06838fdea17484e96f1e4e37683c027
James <4589250+Naught0@users.noreply.github.com>
2024-01-09 19:38:14 -0500

    Add cors (#27)

--- server/app/app.py
+++ server/app/app.py
@@ -1,7 +1,19 @@
 from app.routes import api
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "https://mtgcombinator.com",
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(api.router)

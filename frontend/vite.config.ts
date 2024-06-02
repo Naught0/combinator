4be2b6ee739547d015091564e5423ed2e19cd308
James E <4589250+Naught0@users.noreply.github.com>
2024-06-01 23:40:55 -0400

    Add proxy for running out of docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,11 @@ export default defineConfig({
   server: {
     open: false,
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+      },
+    },
   },
   esbuild: {
     target: "esnext",

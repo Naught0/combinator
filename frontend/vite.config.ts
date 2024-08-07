6380f12062b51c05c02b27eaade02502995951a5
James E <4589250+Naught0@users.noreply.github.com>
2024-08-06 23:14:37 -0400

    Don't be evil

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import eslint from "vite-plugin-eslint";
-import { VitePluginRadar } from "vite-plugin-radar";
 
 export default defineConfig({
   base: "",
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    eslint(),
-    VitePluginRadar({
-      analytics: {
-        id: "G-N2BPB5VE3D",
-      },
-    }),
-  ],
+  plugins: [react(), viteTsconfigPaths(), eslint()],
   server: {
     open: false,
     port: 3000,

eaaebdce38dfb6cb52a354ddbdaeb488db7b1470
James <4589250+Naught0@users.noreply.github.com>
2024-11-21 01:33:51 -0500

    Narrow origins, methods (#52)

--- server/app/app.py
+++ server/app/app.py
@@ -5,8 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
+    allow_origins=["https://mtgcombinator.com"],
+    allow_methods=["GET,POST"],
     allow_headers=["*"],
 )
 

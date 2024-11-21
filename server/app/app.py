5a47e883c4d6321c2be1446bed77514e531d69b6
James E <4589250+Naught0@users.noreply.github.com>
2024-11-21 02:01:09 -0500

    Fix cors in dev

--- server/app/app.py
+++ server/app/app.py
@@ -1,11 +1,18 @@
-from app.routes import api
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.routes import api
+
 app = FastAPI()
+origins = ["https://mtgcombinator.com"]
+if os.getenv("ENV", "").startswith("dev"):
+    origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mtgcombinator.com"],
+    allow_origins=origins,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )

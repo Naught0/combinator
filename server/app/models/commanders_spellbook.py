754130d1c83d52f59571011a6fdb93079fca6b68
James E <4589250+Naught0@users.noreply.github.com>
2024-11-20 21:27:21 -0500

    Relax the schema

--- server/app/models/commanders_spellbook.py
+++ server/app/models/commanders_spellbook.py
@@ -4,100 +4,114 @@ from pydantic import BaseModel
 from typing import List, Any
 from uuid import UUID
 
+
 class IncludeOrOf(BaseModel):
-    id: int
+    id: int | None
+
 
 class Card(BaseModel):
-    id: int
-    name: str
-    spoiler: bool
-    oracleId: UUID
+    id: int | None
+    name: str | None
+    spoiler: bool | None
+    oracleId: UUID | None
+
 
 class Use(BaseModel):
-    card: Card
-    quantity: int
-    zoneLocations: List[str]
-    exileCardState: str
-    mustBeCommander: bool
-    libraryCardState: str
-    graveyardCardState: str
-    battlefieldCardState: str
+    card: Card | None
+    quantity: int | None
+    zoneLocations: List[str] | None
+    exileCardState: str | None
+    mustBeCommander: bool | None
+    libraryCardState: str | None
+    graveyardCardState: str | None
+    battlefieldCardState: str | None
+
 
 class Prices(BaseModel):
-    tcgplayer: str
-    cardmarket: str
-    cardkingdom: str
+    tcgplayer: str | None
+    cardmarket: str | None
+    cardkingdom: str | None
+
 
 class Feature(BaseModel):
-    id: int
-    name: str
-    uncountable: bool
+    id: int | None
+    name: str | None
+    uncountable: bool | None
+
 
 class Produce(BaseModel):
-    feature: Feature
-    quantity: int
+    feature: Feature | None
+    quantity: int | None
+
 
 class Template(BaseModel):
-    id: int
-    name: str
-    scryfallApi: str
-    scryfallQuery: str
+    id: int | None
+    name: str | None
+    scryfallApi: str | None
+    scryfallQuery: str | None
+
 
 class Require(BaseModel):
-    quantity: int
-    template: Template
-    zoneLocations: List[str]
-    exileCardState: str
-    mustBeCommander: bool
-    libraryCardState: str
-    graveyardCardState: str
-    battlefieldCardState: str
+    quantity: int | None
+    template: Template | None
+    zoneLocations: List[str] | None
+    exileCardState: str | None
+    mustBeCommander: bool | None
+    libraryCardState: str | None
+    graveyardCardState: str | None
+    battlefieldCardState: str | None
+
 
 class Legalities(BaseModel):
-    brawl: bool
-    predh: bool
-    legacy: bool
-    modern: bool
-    pauper: bool
-    pioneer: bool
-    vintage: bool
-    standard: bool
-    commander: bool
-    oathbreaker: bool
-    pauperCommander: bool
-    pauperCommanderMain: bool
+    brawl: bool | None
+    predh: bool | None
+    legacy: bool | None
+    modern: bool | None
+    pauper: bool | None
+    pioneer: bool | None
+    vintage: bool | None
+    standard: bool | None
+    commander: bool | None
+    oathbreaker: bool | None
+    pauperCommander: bool | None
+    pauperCommanderMain: bool | None
+
 
 class IncludedOrAlmostIncludedOrAlmostIncludedByAddingColor(BaseModel):
-    id: str
-    of: List[IncludeOrOf]
-    uses: List[Use]
-    notes: str
-    prices: Prices
-    status: str
-    spoiler: bool
-    identity: str
-    includes: List[IncludeOrOf]
-    produces: List[Produce]
-    requires: List[Require]
-    legalities: Legalities
-    popularity: int
-    description: str
-    manaNeeded: str
-    variantCount: int
-    manaValueNeeded: int
-    otherPrerequisites: str
+    id: str | None
+    of: List[IncludeOrOf] | None
+    uses: List[Use] | None
+    notes: str | None
+    prices: Prices | None
+    status: str | None
+    spoiler: bool | None
+    identity: str | None
+    includes: List[IncludeOrOf] | None
+    produces: List[Produce] | None
+    requires: List[Require] | None
+    legalities: Legalities | None
+    popularity: int | None
+    description: str | None
+    manaNeeded: str | None
+    variantCount: int | None
+    manaValueNeeded: int | None
+    otherPrerequisites: str | None
+
 
 class Results(BaseModel):
     identity: str
     included: List[IncludedOrAlmostIncludedOrAlmostIncludedByAddingColor]
     includedByChangingCommanders: List[Any]
     almostIncluded: List[IncludedOrAlmostIncludedOrAlmostIncludedByAddingColor]
-    almostIncludedByAddingColors: List[IncludedOrAlmostIncludedOrAlmostIncludedByAddingColor]
+    almostIncludedByAddingColors: List[
+        IncludedOrAlmostIncludedOrAlmostIncludedByAddingColor
+    ]
     almostIncludedByChangingCommanders: List[Any]
     almostIncludedByAddingColorsAndChangingCommanders: List[Any]
 
+
 class SearchResponse(BaseModel):
-    count: int
+    count: int | None
     next: None
     previous: None
-    results: Results
+    results: Results | None

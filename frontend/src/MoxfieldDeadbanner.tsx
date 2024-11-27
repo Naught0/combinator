a4d790a070b5ea80f12984ff7ae05d7b3b1fdfb3
James E <4589250+Naught0@users.noreply.github.com>
2024-11-26 23:25:46 -0500

    Create moxfield-is-dead banner

---
+++ frontend/src/MoxfieldDeadbanner.tsx
@@ -0,0 +1,11 @@
+export function MoxfieldDeadbanner() {
+  return (
+    <div className="bg-rose-400/30 text-sm px-5 py-3 rounded-lg flex flex-col items-center justify-center text-center">
+      <p className="font-bold">Moxfield has blocked access to their APIs</p>
+      <p>
+        You may use other sources or paste in your list until this is resolved
+      </p>
+      <small>Sorry for the inconvenience :(</small>
+    </div>
+  );
+}

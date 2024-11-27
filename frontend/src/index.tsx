0182c1c446cda1f67c027734f99e16e2c1d42fee
James E <4589250+Naught0@users.noreply.github.com>
2024-11-27 14:05:41 -0500

    Prepare shadcn and update vite, eslint

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./style/style.sass";
+import "./style/index.css";
 import "mana-font";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";

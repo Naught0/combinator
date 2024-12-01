ad11e859b2b27920a20eb8e3a09f53c8c142a6a4
James E <4589250+Naught0@users.noreply.github.com>
2024-12-01 00:27:13 -0500

    Add query client provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import "./style/index.css";
-import "mana-font";
-import reportWebVitals from "./reportWebVitals";
-import { RecoilRoot } from "recoil";
 import "@fontsource-variable/inter";
 import "@fontsource-variable/josefin-sans";
+import "mana-font";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./style/index.css";
 
+const queryClient = new QueryClient();
 const elem = document.getElementById("root");
 if (elem) {
   const root = createRoot(elem);
   root.render(
     <React.StrictMode>
       <RecoilRoot>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </RecoilRoot>
     </React.StrictMode>,
   );

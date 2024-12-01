46a53ab2613f47531ab6e4996e6b9ce500ec52f1
James E <4589250+Naught0@users.noreply.github.com>
2024-12-01 03:17:39 -0500

    Do not cache in axios

--- frontend/src/services/cachedRequest.ts
+++ frontend/src/services/cachedRequest.ts
@@ -1,11 +1,3 @@
 import axios from "axios";
-import { setupCache } from "axios-cache-interceptor";
-
-const instance = axios.create();
-const cachedClient = setupCache(instance, {
-  methods: ["post", "get"],
-  interpretHeader: true,
-  ttl: 1 * 60 * 1000, // 1 min cache
-});
-
-export { cachedClient };
+// NOTE: caching now takes place in react-query
+export const cachedClient = axios.create();

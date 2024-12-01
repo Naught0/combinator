3108049d7f1cb5c0519a28e1c73b76c828b1bad0
James E <4589250+Naught0@users.noreply.github.com>
2024-12-01 02:30:12 -0500

    Create form component

---
+++ frontend/src/Form.tsx
@@ -0,0 +1,33 @@
+import NiceButton from "./NiceButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+export function Form({
+  onSubmit,
+  children,
+  disabled,
+  loading,
+}: React.PropsWithChildren & {
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  disabled?: boolean;
+  loading?: boolean;
+}) {
+  return (
+    <form className="flex max-w-96 flex-col gap-3" onSubmit={onSubmit}>
+      {children}
+      <div>
+        <NiceButton
+          className={"inline-flex justify-center gap-2"}
+          type="submit"
+          disabled={disabled || loading}
+          loading={loading}
+        >
+          <span>think for me</span>{" "}
+          <span>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </span>
+        </NiceButton>
+      </div>
+    </form>
+  );
+}

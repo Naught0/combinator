785fcc1929005692d77ae06b727117130797ba1a
James E <4589250+Naught0@users.noreply.github.com>
2024-12-01 03:17:31 -0500

    Add errors for inputs

---
+++ frontend/src/Field.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Field({
+  children,
+  error,
+}: {
+  children: React.ReactNode;
+  error?: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col gap-1">
+      {children} {error && <small className="text-red-400">{error}</small>}
+    </div>
+  );
+}

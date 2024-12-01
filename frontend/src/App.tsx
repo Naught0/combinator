af2615c666b6e6a8406ccd19ebd427a04871812c
James E <4589250+Naught0@users.noreply.github.com>
2024-12-01 04:17:37 -0500

    Split out paste deck url page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,17 @@
-import { Input } from "@/components/ui/input";
 import "@fontsource-variable/inter";
 import "@fontsource-variable/josefin-sans";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import { ComboContainer } from "./ComboContainer";
 import { Footer } from "./Footer";
-import { useComboData } from "./hooks/useComboData";
 import MoxfieldSearch from "./MoxfieldSearch";
 import Nav from "./Nav";
 import { PasteList } from "./PasteList";
 import { SearchType, SearchTypeSelector } from "./SearchTypeSelector";
+import { PasteDeckUrl } from "./PasteDeckUrl";
 
 export const App = () => {
-  const [deckUrl, setDeckUrl] = useState("");
   const [searchType, setSearchType] = useState(SearchType.MOXFIELD_USER);
-  const { comboData, errorMessage: comboError, getUrl } = useComboData();
-
-  useEffect(function consumeUrlParams() {
-    const params = new URLSearchParams(window.location.search);
-    const url = params.get("deck_url");
-    const searchType = localStorage.getItem("searchType");
-    if (url) {
-      setDeckUrl(url);
-      setSearchType(SearchType.DECK);
-    }
-
-    if (searchType) {
-      setSearchType(SearchType[searchType as SearchType]);
-    }
-  }, []);
 
   const saveSearchType = (type: SearchType) => {
     localStorage.setItem("searchType", type);
@@ -46,29 +28,11 @@ export const App = () => {
             searchType={searchType}
             setSearchType={saveSearchType}
           />
-          <div className="flex max-w-96 flex-col gap-3">
+          <div className="flex w-full flex-col gap-3">
             {searchType === SearchType.PASTE && <PasteList />}
-            {searchType === SearchType.DECK && (
-              <>
-                <Input
-                  type="text"
-                  className={`w-full rounded border border-zinc-500 bg-transparent px-3 py-2 ${comboError ? "is-danger" : ""}`}
-                  placeholder="Moxfield, Archidekt, or MTGGoldfish deck URL"
-                  onInput={(e) => {
-                    setDeckUrl((e.target as HTMLInputElement).value);
-                  }}
-                  value={deckUrl}
-                />
-                {comboError && (
-                  <p className="has-text-danger help">{comboError}</p>
-                )}
-              </>
-            )}
+            {searchType === SearchType.DECK && <PasteDeckUrl />}
             {searchType === SearchType.MOXFIELD_USER && <MoxfieldSearch />}
           </div>
-          {comboData && searchType !== SearchType.MOXFIELD_USER && (
-            <ComboContainer />
-          )}
         </div>
       </div>
       <Footer />
---
+++ frontend/src/PasteDeckUrl.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Input } from "./components/ui/input";
+import { useQuery } from "react-query";
+import { getDeckData } from "./services";
+import { Field } from "./Field";
+import type { AxiosError } from "axios";
+import { TabContainer } from "./TabContainer";
+import { Form } from "./Form";
+import { deckDataAtom } from "./atoms";
+import { useSetRecoilState } from "recoil";
+
+export function PasteDeckUrl() {
+  const [deckUrl, setDeckUrl] = useState("");
+  const [enabled, setEnabled] = useState(false);
+  const setDeckData = useSetRecoilState(deckDataAtom);
+  const { data, error, isLoading } = useQuery<DeckData, AxiosError>(
+    ["deck-data", deckUrl],
+    () => getDeckData(deckUrl),
+    {
+      enabled,
+      onSettled() {
+        setEnabled(false);
+      },
+    },
+  );
+  useEffect(
+    function setDeckDataAtom() {
+      setDeckData(data);
+    },
+    [data],
+  );
+  return (
+    <TabContainer>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setEnabled(true);
+        }}
+        loading={isLoading}
+      >
+        <Field error={error && "Unable to find deck"}>
+          <Input
+            type="text"
+            variant={error ? "error" : "default"}
+            placeholder="Moxfield, Archidekt, or MTGGoldfish deck URL"
+            onInput={(e) => {
+              setDeckUrl((e.target as HTMLInputElement).value);
+            }}
+            value={deckUrl}
+          />
+        </Field>
+      </Form>
+    </TabContainer>
+  );
+}

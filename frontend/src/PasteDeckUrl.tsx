ed5467ccb781fb3c71df96707cdf4fd0b83a3a68
James E <4589250+Naught0@users.noreply.github.com>
2024-12-01 22:27:05 -0500

    Upgrade react query syntax

--- frontend/src/PasteDeckUrl.tsx
+++ frontend/src/PasteDeckUrl.tsx
@@ -1,31 +1,44 @@
 import { useEffect, useState } from "react";
 import { Input } from "./components/ui/input";
-import { useQuery } from "react-query";
-import { getDeckData } from "./services";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getComboData, getDeckData } from "./services";
 import { Field } from "./Field";
 import type { AxiosError } from "axios";
 import { TabContainer } from "./TabContainer";
 import { Form } from "./Form";
-import { deckDataAtom } from "./atoms";
-import { useSetRecoilState } from "recoil";
+import { comboDataAtom, deckDataAtom } from "./atoms";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { ComboContainer } from "./ComboContainer";
 
 export function PasteDeckUrl() {
   const [deckUrl, setDeckUrl] = useState("");
   const [enabled, setEnabled] = useState(false);
+  const [comboData, setComboData] = useRecoilState(comboDataAtom);
+  const queryClient = useQueryClient();
   const setDeckData = useSetRecoilState(deckDataAtom);
-  const { data, error, isLoading } = useQuery<DeckData, AxiosError>(
-    ["deck-data", deckUrl],
-    () => getDeckData(deckUrl),
-    {
-      enabled,
-      onSettled() {
-        setEnabled(false);
-      },
-    },
-  );
+  const { data, error, isLoading } = useQuery<DeckData, AxiosError>({
+    queryKey: ["deck-data", deckUrl],
+    queryFn: () => getDeckData(deckUrl),
+    enabled,
+  });
   useEffect(
     function setDeckDataAtom() {
-      setDeckData(data);
+      if (!data) return;
+
+      (async () => {
+        setDeckData(data);
+        setEnabled(false);
+
+        const d = await queryClient.fetchQuery({
+          queryKey: ["combo-data", data.meta.url],
+          queryFn: async () =>
+            await getComboData({
+              commanders: [],
+              main: data.cards.map((c) => ({ card: c.name, quantity: 1 })),
+            }),
+        });
+        setComboData(d);
+      })();
     },
     [data],
   );
@@ -51,6 +64,13 @@ export function PasteDeckUrl() {
           />
         </Field>
       </Form>
+      {data && comboData && (
+        <ComboContainer
+          deckData={data}
+          allCombos={comboData}
+          cardNames={data.cards.map((c) => c.name)}
+        />
+      )}
     </TabContainer>
   );
 }

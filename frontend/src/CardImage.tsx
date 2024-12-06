4f63f173f495fb67a6e3a0afe3602fb7805e75b7
James E <4589250+Naught0@users.noreply.github.com>
2024-12-05 23:27:44 -0500

    Lazy load

--- frontend/src/CardImage.tsx
+++ frontend/src/CardImage.tsx
@@ -1,7 +1,11 @@
 export const CardImage = (props: { cardImage: string }) => {
   return (
-    <div className="bg-transparent !rounded-2xl">
-      <img src={props.cardImage} className="rounded-2xl w-full" />
+    <div className="!rounded-2xl bg-transparent">
+      <img
+        src={props.cardImage}
+        className="w-full rounded-2xl"
+        loading="lazy"
+      />
     </div>
   );
 };

3174b2d876eba25431abc99ec240e207fe5cdcba
James E <4589250+Naught0@users.noreply.github.com>
2024-12-05 22:19:26 -0500

    Add tab instructions

---
+++ frontend/src/TabInstructions.tsx
@@ -0,0 +1,13 @@
+export function TabInstructions() {
+  return (
+    <>
+      <p className="text-sm">
+        Click a combo to see its prerequisites and steps
+      </p>
+      <p className="text-sm">
+        If more combos are found by adding a card to the deck, you can access
+        them by clicking the &quot;Add 1&quot; tab
+      </p>
+    </>
+  );
+}

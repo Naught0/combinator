d851835c59a1e282c59ef814011d08875df28336
James E <4589250+Naught0@users.noreply.github.com>
2024-12-05 22:18:40 -0500

    Update paste list logic

--- frontend/src/PasteList.tsx
+++ frontend/src/PasteList.tsx
@@ -1,39 +1,66 @@
-import { useRecoilState } from "recoil";
-import { useDebouncedCallback } from "use-debounce";
-import { pastedDeckListAtom } from "./atoms";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useMemo, useState } from "react";
+import { useDebounce } from "use-debounce";
+import { ComboTabs } from "./ComboTabs";
 import { Textarea } from "./components/ui/textarea";
 import { Field } from "./Field";
 import { Form } from "./Form";
+import { getCardData } from "./services";
 import { TabContainer } from "./TabContainer";
-import { useNavigate, useParams } from "react-router";
-import { useEffect } from "react";
+import { DeckInfo } from "./DeckInfo";
+
+const DECK_META: DeckMeta = { name: "Pasted Deck", author: "You", url: "" };
 
 export const PasteList = () => {
-  const { pastedList: urlPastedList } = useParams<{ pastedList: string }>();
-  const [pastedList, setPastedList] = useRecoilState(pastedDeckListAtom);
-  const persistList = useDebouncedCallback(() => {
-    localStorage.setItem("pastedList", pastedList);
-  }, 500);
-  let navigate = useNavigate();
-  useEffect(
-    function initState() {
-      if (urlPastedList) {
-        return setPastedList(urlPastedList);
-      }
-      const localPastedList = localStorage.getItem("pastedList");
-      if (localPastedList) {
-        return setPastedList(localPastedList);
-      }
-    },
-    [urlPastedList],
+  const [pastedList, setPastedList] = useState(
+    localStorage.getItem("pastedList") ?? "",
   );
+  const [enabled, setEnabled] = useState(false);
+  const [deckHash, setDeckHash] = useState("");
+  const [debouncedList] = useDebounce(pastedList, 500);
+  useEffect(
+    function persistList() {
+      localStorage.setItem("pastedList", pastedList);
+    },
+    [debouncedList],
+  );
+  const { data, isError, isSuccess } = useQuery({
+    queryKey: ["pasted-list", deckHash],
+    queryFn: async () => getCardData(parseCardList(pastedList)),
+    enabled,
+  });
+  useEffect(
+    function disableQuery() {
+      setEnabled(false);
+    },
+    [isError, isSuccess],
+  );
+  useEffect(
+    function calculateHash() {
+      if (!debouncedList) return;
+
+      (async () => {
+        const hash = await generateDeckIdHash(parseCardList(debouncedList));
+        setDeckHash(hash);
+      })();
+    },
+    [debouncedList],
+  );
+  const deckData = useMemo<DeckData>(() => {
+    return {
+      id: deckHash,
+      source: "paste" as DeckSource,
+      cards: data?.cards ?? [],
+      meta: DECK_META,
+    };
+  }, [data?.cards, deckHash]);
 
   return (
     <TabContainer>
       <Form
         onSubmit={(e) => {
           e.preventDefault();
-          navigate(`/paste/${encodeURIComponent(pastedList)}`);
+          setEnabled(true);
         }}
         disabled={!pastedList}
       >
@@ -44,13 +71,18 @@ export const PasteList = () => {
             }
             onChange={(e) => {
               setPastedList(e.target.value);
-              persistList();
             }}
             value={pastedList || ""}
             className="h-36 max-h-[512px] min-h-36 rounded p-2"
           ></Textarea>
         </Field>
       </Form>
+      {deckData && (
+        <>
+          <DeckInfo deckData={deckData} />
+          <ComboTabs deckData={deckData} />
+        </>
+      )}
     </TabContainer>
   );
 };
@@ -68,4 +100,18 @@ export function parseCardList(list: string) {
     .filter((c) => c)
     .map((c) => c.replace(/^\dx?\s*/, ""))
     .slice(0, MAX_CARDS);
+}
+
+async function generateDeckIdHash(cards: string[]) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+  const hashBuffer = await crypto.subtle.digest(
+    "sha-1",
+    new TextEncoder().encode(cards.join("\n")),
+  );
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join(""); // convert bytes to hex string
+
+  return hashHex;
 }

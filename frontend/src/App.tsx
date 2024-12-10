9a2593b89e8f37cccae07334b541d8b21b4e7b20
James E <4589250+Naught0@users.noreply.github.com>
2024-12-09 22:34:05 -0500

    Improve container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,11 @@ import { Outlet } from "react-router";
 
 export const App = () => {
   return (
-    <main className="flex h-full w-full flex-col items-center bg-zinc-800 px-3 py-6 text-zinc-100">
+    <main className="flex h-full min-h-screen w-full flex-col items-center bg-zinc-800 text-zinc-100">
       <ToastContainer theme="dark" />
-      <div className="flex min-h-screen w-full flex-col items-center gap-6 md:max-w-screen-lg md:gap-12 xl:max-w-screen-lg">
+      <div className="flex min-h-screen w-full flex-grow flex-col items-center gap-6 px-3 py-6 md:max-w-screen-lg md:gap-12 xl:max-w-screen-lg">
         <Nav />
-        <div className="flex w-full flex-col gap-3">
-          <Outlet />
-        </div>
+        <Outlet />
       </div>
       <Footer />
     </main>

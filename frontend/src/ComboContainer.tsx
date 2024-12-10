6e23aa9d86e955c7cf6a0b0a1cb597cbd6c3025c
James E <4589250+Naught0@users.noreply.github.com>
2024-12-10 00:10:42 -0500

    Display errors

--- frontend/src/ComboContainer.tsx
+++ frontend/src/ComboContainer.tsx
@@ -4,6 +4,7 @@ import { getDeckById } from "./services";
 import { AxiosError } from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { DeckInfo } from "./DeckInfo";
+import { Error } from "./Error";
 
 export const ComboContainer = ({
   deckId,
@@ -12,17 +13,34 @@ export const ComboContainer = ({
   deckId: string;
   source: DeckSource;
 }) => {
-  const { data: deckData, isLoading } = useQuery<DeckData, AxiosError>({
+  const {
+    data: deckData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<DeckData, AxiosError>({
     queryKey: ["deck-data", { id: deckId, source }],
     queryFn: () => getDeckById(source, deckId),
   });
   const { meta } = deckData || {};
+  if (error?.response?.status === 404)
+    return <Error message={"Deck not found"} />;
 
   return (
     <div className="flex flex-col gap-3">
       {isLoading && <Loading size="lg" message="Loading deck" />}
       {meta && <DeckInfo meta={meta} />}
       {deckData && <ComboTabs deckData={deckData} />}
+      {isError && (
+        <Error
+          code={error?.response?.status || 500}
+          message={
+            typeof error?.response?.data === "string"
+              ? error.response.data
+              : "Something went wrong"
+          }
+        />
+      )}
     </div>
   );
 };

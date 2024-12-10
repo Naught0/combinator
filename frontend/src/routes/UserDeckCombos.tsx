112efbaaead68a345cdf2c4df16d97fcbaddb00b
James E <4589250+Naught0@users.noreply.github.com>
2024-12-10 00:10:42 -0500

    Display errors

--- frontend/src/routes/UserDeckCombos.tsx
+++ frontend/src/routes/UserDeckCombos.tsx
@@ -1,20 +1,25 @@
-import { NotFound } from "@/404";
 import { ComboContainer } from "@/ComboContainer";
+import { Error } from "@/Error";
 import { IconText } from "@/IconText";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { PropsWithChildren } from "react";
 import { Link, useParams } from "react-router";
 
 export function UserDeckCombos({ source }: { source: DeckSource }) {
   let { userId, deckId } = useParams();
-  if (!userId) return <NotFound what={"User"} />;
-  if (!deckId) return <NotFound what={"Deck"} />;
+  if (!userId) return <Error message={"User not found"} />;
+  if (!deckId) return <Error message={"Deck not found"} />;
 
   return (
-    <div className="flex flex-1 flex-col gap-3">
+    <Container>
       <Link to={`/user/${source}/${userId}`}>
         <IconText icon={faArrowLeft}>Back to decks</IconText>
       </Link>
       <ComboContainer deckId={deckId} source={source} />
-    </div>
+    </Container>
   );
+}
+
+function Container({ children }: PropsWithChildren) {
+  return <article className="flex flex-1 flex-col gap-3">{children}</article>;
 }

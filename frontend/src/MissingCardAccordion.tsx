ccb81496b88651606ca855d7841f6daee2e2d50a
James E <4589250+Naught0@users.noreply.github.com>
2024-12-16 01:37:43 -0500

    Update styling, spacing

--- frontend/src/MissingCardAccordion.tsx
+++ frontend/src/MissingCardAccordion.tsx
@@ -8,9 +8,7 @@ export const MissingCardAccordion = (props: {
 }) => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <div
-      className={`flex basis-1/2 flex-col rounded-lg transition-colors ${expanded ? "bg-[rgba(0,0,0,0.25)]" : "hover:bg-[rgba(0,0,0,0.15)]"} `}
-    >
+    <div className={`flex flex-col`}>
       <div
         className={`flex flex-1 cursor-pointer flex-row items-center gap-3 rounded-md p-5`}
         onClick={() => setExpanded(!expanded)}
@@ -24,7 +22,7 @@ export const MissingCardAccordion = (props: {
           ({props.count})
         </div>
       </div>
-      {expanded && <div className="flex flex-col gap-6">{props.children}</div>}
+      {expanded && <div className="flex flex-col gap-3">{props.children}</div>}
     </div>
   );
 };

f097379f59bc9512a5e7583cf27727be7dfdeb4c
James E <4589250+Naught0@users.noreply.github.com>
2024-12-15 23:18:52 -0500

    Fix attrs

--- frontend/src/Waves.tsx
+++ frontend/src/Waves.tsx
@@ -10,30 +10,30 @@ export function Waves() {
     >
       <defs>
         <linearGradient id="gradient" x1="50%" y1="0%" x2="50%" y2="100%">
-          <stop offset="5%" stop-color="#3f3f46"></stop>
-          <stop offset="95%" stop-color="#18181b"></stop>
+          <stop offset="5%" stopColor="#3f3f46"></stop>
+          <stop offset="95%" stopColor="#18181b"></stop>
         </linearGradient>
       </defs>
       <path
         d="M 0,400 L 0,100 C 141.21428571428572,82.82142857142857 282.42857142857144,65.64285714285714 411,65 C 539.5714285714286,64.35714285714286 655.5,80.25 747,99 C 838.5,117.75 905.5714285714287,139.35714285714286 1017,140 C 1128.4285714285713,140.64285714285714 1284.2142857142858,120.32142857142857 1440,100 L 1440,400 L 0,400 Z"
         stroke="none"
-        stroke-width="0"
+        strokeWidth="0"
         fill="url(#gradient)"
-        fill-opacity="0.53"
+        fillOpacity="0.53"
         className="path-0 transition-all delay-150 duration-300 ease-in-out"
       ></path>
       <defs>
         <linearGradient id="gradient" x1="50%" y1="0%" x2="50%" y2="100%">
-          <stop offset="5%" stop-color="#3f3f46"></stop>
-          <stop offset="95%" stop-color="#18181b"></stop>
+          <stop offset="5%" stopColor="#3f3f46"></stop>
+          <stop offset="95%" stopColor="#18181b"></stop>
         </linearGradient>
       </defs>
       <path
         d="M 0,400 L 0,233 C 89.85714285714286,247.82142857142858 179.71428571428572,262.64285714285717 311,260 C 442.2857142857143,257.35714285714283 614.9999999999999,237.24999999999997 757,222 C 899.0000000000001,206.75000000000003 1010.2857142857144,196.35714285714286 1119,199 C 1227.7142857142856,201.64285714285714 1333.8571428571427,217.32142857142856 1440,233 L 1440,400 L 0,400 Z"
         stroke="none"
-        stroke-width="0"
+        strokeWidth="0"
         fill="url(#gradient)"
-        fill-opacity="1"
+        fillOpacity="1"
         className="path-1 transition-all delay-150 duration-300 ease-in-out"
       ></path>
     </svg>

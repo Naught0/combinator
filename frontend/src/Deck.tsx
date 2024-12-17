71efb8eaadab021301fcb903d1f4161441549177
james.euteneuer <james.euteneuer@rechargeapps.com>
2024-12-17 13:01:29 -0500

    Improve information density and icons

--- frontend/src/Deck.tsx
+++ frontend/src/Deck.tsx
@@ -1,14 +1,11 @@
-import {
-  faBalanceScale,
-  faEdit,
-  faEye,
-  faHashtag,
-  faSquarePlus,
-  faThumbsUp,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { BsExclamationTriangleFill } from "react-icons/bs";
+import { TbCardsFilled } from "react-icons/tb";
 import { FC } from "react";
 import { Link } from "react-router";
 import { IconText } from "./IconText";
+import { Badge } from "./components/ui/badge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Props {
   deck: Deck;
@@ -18,10 +15,10 @@ export const Deck: FC<Props> = ({ deck }) => {
   return (
     <Link
       to={`/user/moxfield/${deck.authors[0].userName}/deck/${deck.publicId}`}
-      className="text-zinc-100 no-underline visited:text-zinc-200 hover:text-white hover:no-underline"
+      className="text-zinc-100 visited:text-zinc-200 hover:text-white hover:no-underline"
     >
       <div
-        className="flex flex-col justify-between rounded bg-zinc-900 bg-no-repeat p-4"
+        className="flex flex-col justify-between gap-2 rounded bg-zinc-950 bg-no-repeat p-4"
         style={{
           backgroundImage: deck.mainCardId
             ? `radial-gradient(transparent, rgb(0, 0, 0)),
@@ -29,40 +26,41 @@ export const Deck: FC<Props> = ({ deck }) => {
             : "",
           backgroundSize: "100%",
         }}
-        title={deck.name}
       >
-        <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap font-serif text-lg">
           {deck.name}
+        </p>
+        <div>
+          <Badge className="text-sm capitalize" title={"deck format"}>
+            {deck.format}
+          </Badge>
         </div>
-        <div className="tags my-2">
-          <span className="tag is-dark">{deck.format}</span>
-        </div>
-        <div className="flex flex-row flex-wrap gap-2">
-          <IconText className="text-sm" icon={faHashtag}>
-            {deck.mainboardCount}{" "}
-            {deck.mainboardCount > 1 || deck.mainboardCount === 0
-              ? "cards"
-              : "card"}
-          </IconText>
-          <IconText className="text-sm" icon={faBalanceScale}>
+        <div className="flex flex-row flex-wrap gap-1">
+          <Badge title={"# cards in deck"}>
+            <div className="5 inline-flex items-center gap-1">
+              <TbCardsFilled />
+              {deck.mainboardCount}
+            </div>
+          </Badge>
+          <Badge
+            className={`inline-flex items-center gap-1.5 ${deck.isLegal ? "" : "!text-rose-300"}`}
+          >
             {deck.isLegal ? (
-              "legal"
+              <FontAwesomeIcon icon={faCheck} />
             ) : (
-              <span className="has-text-danger">not legal</span>
+              <BsExclamationTriangleFill />
             )}
-          </IconText>
-          <IconText className="text-sm" icon={faThumbsUp}>
-            {deck.likeCount} likes
-          </IconText>
-          <IconText className="text-sm" icon={faEye}>
-            {deck.viewCount} views
-          </IconText>
-          <IconText className="text-sm" icon={faSquarePlus}>
-            created {new Date(deck.createdAtUtc).toLocaleDateString()}
-          </IconText>
-          <IconText className="text-sm" icon={faEdit}>
-            modified {new Date(deck.lastUpdatedAtUtc).toLocaleDateString()}
-          </IconText>
+            {deck.isLegal ? "Legal" : "Not legal"}
+          </Badge>
+          <Badge title={"Last updated"}>
+            <IconText icon={faCalendarDays}>
+              {new Date(deck.lastUpdatedAtUtc).toLocaleDateString(undefined, {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+              })}
+            </IconText>
+          </Badge>
         </div>
       </div>
     </Link>

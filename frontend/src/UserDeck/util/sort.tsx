f96faf70929c2a3074124272b1e758e114a46d2b
James E <4589250+Naught0@users.noreply.github.com>
2024-12-23 01:46:17 -0500

    Forgive me father, for I have broadened my types

--- frontend/src/UserDeck/util/sort.tsx
+++ frontend/src/UserDeck/util/sort.tsx
@@ -2,7 +2,7 @@ import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ReactNode } from "react";
 
-export const sortDirIconMap = new Map<"ascending" | "descending", ReactNode>([
+export const sortDirIconMap = new Map<string, ReactNode>([
   // eslint-disable-next-line react/jsx-key
   ["ascending", <FontAwesomeIcon icon={faArrowUp} />],
   // eslint-disable-next-line react/jsx-key

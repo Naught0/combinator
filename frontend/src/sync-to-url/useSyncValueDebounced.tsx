546fca8945bc298575b9fadc562d7caa9203e43d
James E <4589250+Naught0@users.noreply.github.com>
2025-01-16 18:19:34 -0500

    Remove default value

--- frontend/src/sync-to-url/useSyncValueDebounced.tsx
+++ frontend/src/sync-to-url/useSyncValueDebounced.tsx
@@ -6,29 +6,20 @@ export function useSyncValueDebounced({
   name,
   value,
   debounceMs,
-  defaultValue,
 }: {
   value: string;
   name: string;
   debounceMs: number;
-  defaultValue?: string;
 }) {
   const [, setParams] = useSearchParams();
   const [debouncedValue] = useDebounce<string>(value, debounceMs);
 
   useEffect(
     function sync() {
-      if (!value || value === defaultValue) {
-        setParams((prev) => {
-          prev.delete(name);
-          return prev;
-        });
-      } else {
-        setParams((prev) => {
-          prev.set(name, value);
-          return prev;
-        });
-      }
+      setParams((prev) => {
+        prev.set(name, value);
+        return prev;
+      });
     },
     [debouncedValue],
   );

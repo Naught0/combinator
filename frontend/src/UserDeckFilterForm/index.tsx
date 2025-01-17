efe5e507f6236d1a16cb9f312761f1b697d032b1
James E <4589250+Naught0@users.noreply.github.com>
2025-01-17 02:10:23 -0500

    Remove zod

--- frontend/src/UserDeckFilterForm/index.tsx
+++ frontend/src/UserDeckFilterForm/index.tsx
@@ -1,23 +1,3 @@
-import { z } from "zod";
-
-const formSchema = z.object({
-  showIllegal: z.coerce.boolean().default(true),
-  authorUserNames: z.array(z.string()).default([]),
-  pageNumber: z.coerce.number().min(1, "Page must be >= 1").default(1),
-  pageSize: z.coerce
-    .number()
-    .min(6, "Page size must be >= 6")
-    .max(46, "Page size must be <= 46")
-    .default(12),
-  sortType: z.coerce.string().default("updated"),
-  sortDirection: z.coerce.string().default("descending"),
-  board: z.coerce.string().default("mainboard"),
-  filter: z.coerce.string().default(""),
-  fmt: z.coerce.string().default("any"),
-});
-
-export type DeckFilterParams = z.infer<typeof formSchema>;
-
 export const defaultFormValues = {
   board: "mainboard",
   filter: "",

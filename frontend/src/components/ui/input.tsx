bfa313804acd5d0256402203a850a7dffc3150eb
James E <4589250+Naught0@users.noreply.github.com>
2025-01-17 00:05:25 -0500

    Remove hook input

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
-import { useFormContext } from "react-hook-form";
 
 const inputVariants = cva(
   "flex h-10 w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-base ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-zinc-950 placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:file:text-zinc-50 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300 md:text-sm",
@@ -36,22 +35,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 );
 Input.displayName = "Input";
 
-const HookInput = ({
-  className,
-  type,
-  name,
-  variant,
-  ...props
-}: InputProps & { name: string }) => {
-  const { register } = useFormContext();
-  return (
-    <input
-      type={type}
-      className={cn(inputVariants({ variant }), className)}
-      {...props}
-      {...register(name)}
-    />
-  );
-};
-
-export { Input, HookInput };
+export { Input };

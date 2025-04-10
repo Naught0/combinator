e5fe4cea05e50addc8ce87ed719607f9787f8b93
James E <4589250+Naught0@users.noreply.github.com>
2025-04-09 22:51:03 -0400

    Add button to return home

--- frontend/src/Error.tsx
+++ frontend/src/Error.tsx
@@ -1,3 +1,6 @@
+import { ArrowLeft } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 export function Error({
   code = 404,
   message = "Not found",
@@ -6,10 +9,19 @@ export function Error({
   message?: string;
 }) {
   return (
-    <article className="flex h-full min-h-full w-full flex-grow items-center justify-center gap-6">
-      <h1 className="text-5xl lg:text-7xl">{code}</h1>
-      <div className="h-24 w-[1px] bg-white lg:h-36"></div>
-      <p className="text-2xl lg:text-4xl">{message}</p>
+    <article className="flex flex-col gap-6">
+      <div className="flex h-full min-h-full w-full flex-grow items-center justify-center gap-6">
+        <h1 className="text-5xl lg:text-7xl">{code}</h1>
+        <div className="h-24 w-[1px] bg-white lg:h-36"></div>
+        <p className="text-2xl lg:text-4xl">{message}</p>
+      </div>
+      <a
+        href="/"
+        className="inline-flex items-center gap-2 self-center text-xl md:text-2xl"
+      >
+        <ArrowLeft />
+        Back Home
+      </a>
     </article>
   );
 }
